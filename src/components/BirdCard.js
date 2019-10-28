c39d0283cff6a531ab6bd2b65c66c6716f75f370
CFD2 <5952749+CFD2@users.noreply.github.com>
2019-10-28 17:56:46 -0400

    add BirdCard.js component

---
+++ src/components/BirdCard.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Image, TouchableHighlight, ScrollView, Alert, Button, TextInput} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+export default class BirdCard extends Component {
+
+
+
+    render() {
+
+    
+        return (
+            <TouchableOpacity onPress={this.props.onPress} onLongPress={this.props.onLongPress} style={[styles.container, this.props.style]}>
+                <View style={styles.container}>
+                    
+                    <Image
+                    source={this.props.imgUrl}
+                    resizeMode="cover"
+                    style={styles.img}
+                />
+                <Text style={styles.birdName}>{this.props.birdName}</Text>
+                    </View>
+            </TouchableOpacity>
+        );
+
+
+    }
+
+
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        borderRadius: 1,
+        borderColor: 'black',
+        borderWidth: 0.1,
+        
+    },
+    img: {
+        width: 120,
+        height: 80,
+        // borderRadius: 2,
+        // borderColor: "black",
+        // borderWidth: 1,
+    },
+    birdName:{
+        padding: 15,
+        textAlignVertical: 'center',
+    },
+});
