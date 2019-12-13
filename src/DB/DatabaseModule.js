95829a80662549d814ecd7a723dd1be4cbfc8d7c
RobynVG <RobynVG21@gmail.com>
2019-12-13 01:15:20 -0500

    ability to update multiple, necessary changes to User table + queries, printTable function, remove unnescessary insert queries

--- src/DB/DatabaseModule.js
+++ src/DB/DatabaseModule.js
@@ -1,26 +1,42 @@
 import { SQLite } from 'expo-sqlite';
 const db = SQLite.openDatabase('Dendroica.db');
 
+const FIELDS = {
+    "User": ["_id","dendroica_id","username","password","email","first_name","last_name","language","naming","sorting"],
+    "VersionData": ["_id","apiVersion","dataVersion","lastUpdateTimeStamp"],
+    "Birds": ["_id","name","scientific_name","range_description","song_description"],
+    "Regions": ["_id","project_id","name"],
+    "Lists": ["_id","name"],
+    "BirdImages": ["_id","bird_id","filename","credits","displayOrder"],
+    "MapImages": ["_id","bird_id","filename","credits"],
+    "Vocalizations": ["_id","bird_id","filename","mp3Filename","credits"],
+    "SubRegions": ["_id","region_id","name","abbrev"],
+    "FileSubRegions": ["subregion_id","file_id","displayOrder"],
+    "BirdSubRegions": ["subregion_id","bird_id","non_breeder","rare"],
+    "BirdRegions": ["region_id","bird_id","non_breeder","rare"],
+    "BirdLists": ["list_id","bird_id",""]
+}
+
 
 //  Inserts username or updates if exists
-var updateUser = function (dendroicaId, username, password, email, firstName, lastName, excludeNonBreeding, excludeRare, language, naming, sorting, callbacks) {
+var updateUser = function (dendroicaId, username, password, email, firstName, lastName, language, naming, sorting, callbacks) {
     var countQuery  = `SELECT COUNT(*) from User`;
     var updateQuery = `UPDATE User
-                       SET _id=?,email=?,first_name=?,last_name=?,exclude_non_breeding=?,exclude_rare=?,language=?,naming=?,sorting=?
-                       WHERE _id = 1`;
+                       SET _id=?,email=?,first_name=?,last_name=?,language=?,naming=?,sorting=?
+                       WHERE _id=1`;
     var insertQuery = `INSERT into User
-                       VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
+                       VALUES (?,?,?,?,?,?,?,?,?,?)`;
 
     _sqlQuery(countQuery, [], { success: (tx,res) => {
         if (res.rows._array[0]["COUNT(*)"] === 0) { //Insert
             _sqlQuery(insertQuery,
-                [1,dendroicaId, username, password, email, firstName, lastName, excludeNonBreeding, excludeRare, language, naming, sorting],
+                [1,dendroicaId, username, password, email, firstName, lastName, language, naming, sorting],
                 callbacks);
         }
 
         if (res.rows._array[0]["COUNT(*)"] === 1) { //Update
             _sqlQuery(updateQuery,
-                [dendroicaId, email, firstName, lastName, excludeNonBreeding, excludeRare, language, naming, sorting],
+                [dendroicaId, email, firstName, lastName, language, naming, sorting],
                 callbacks);
         }
     }});
@@ -46,17 +62,15 @@ var updateVersionData = function(versionDataToUpdate, onFinishedCallback) {
     var countQuery  = "SELECT COUNT(*) from VersionData";
     var insertQuery = "INSERT INTO VersionData VALUES (?,?,?,?)";
     var updateQuery = "UPDATE VersionData SET apiVersion=?, dataVersion=?, lastUpdateTimeStamp=? where _id=1";
-    var updateParams= [];
+    var updateParams= [versionDataToUpdate.apiVersion, versionDataToUpdate.dataVersion, versionDataToUpdate.serverTimeStamp];
 
     _sqlQuery(countQuery, [], {success: (tx,res) => {
         if (res.rows._array[0]["COUNT(*)"] === 1) {
-            console.log("updating version data");
             _sqlQuery(updateQuery, updateParams, { success: (tx,res) => {
                 onFinishedCallback();
             }});
         }
         if (res.rows._array[0]["COUNT(*)"] === 0) {
-            console.log("inserting version data");
             var params = [1,versionDataToUpdate.apiVersion, versionDataToUpdate.dataVersion, versionDataToUpdate.serverTimeStamp];
             _sqlQuery(insertQuery, params, {success: (tx,res) => {
                 onFinishedCallback();
@@ -106,28 +120,80 @@ var insertMultiple = function (json, jsonArrayNames, tableName, onFinishedCallba
     });
 }
 
+var upsertMultiple = function (json, jsonArrayNames, tableName, onFinishedCallback) {
+    var countQuery = "SELECT COUNT(*) from " + tableName + " WHERE _id=";
+    var insertQuery = "INSERT INTO " + tableName + " VALUES (";
+    var updateQuery = "UPDATE " + tableName + " SET ";
+
+    FIELDS[tableName].forEach(field => {
+        insertQuery += "?,";
+        if (field !== "_id") updateQuery += field + "=?,";
+    })
+    insertQuery = insertQuery.slice(0,-1) + ")";
+    updateQuery = updateQuery.slice(0,-1) + " WHERE _id=";
+
+    var idArray = json[jsonArrayNames[0]];
+    db.transaction((tx) => {
+        var i,index,innerIndex;
+        for (var i = 0, index = 0, innerIndex = 0; i < idArray.length; i++) {
+            var params = [];
+            var updateQueryWithId = updateQuery + idArray[i];
+            _sqlQuery(countQuery + idArray[i], [], {success: (tx,res) => {
+                var isInsertQuery = false;
+                var query = "";
+                var params = [];
+
+                if (res.rows._array[0]["COUNT(*)"] === 0) isInsertQuery = true;
+
+                if (isInsertQuery) query = insertQuery;
+                else query = updateQuery + idArray[index];
+
+                //build params array
+                jsonArrayNames.forEach(arrayName => {
+                    if (! (arrayName === "id" && !isInsertQuery)) //Don't push id for update query
+                    params.push(json[arrayName][index]);
+                })
+
+                _sqlQuery(query, params, {success: (tx,res) => {
+                    if (++innerIndex === idArray.length) onFinishedCallback();
+                }},tx);
+
+                index ++;
+            }}, tx);
+        }
+    });
+}
+
 //  ["regionId","speciesId","nonBreederInRegion","rareInRegion"]
 //  region_id integer REFERENCES Regions(_id), bird_id integer REFERENCES Birds(_id), non_breeder boolean, rare boolean
 //  Function to create both BirdRegions table and SubRegions table as they are populated from the same json
-var insertBirdRegionsAndBirdSubRegions = function (json, regionIds, onFinishedCallback) {
+var updateInsertBirdRegionsAndBirdSubRegions = function (json, regionIds, onFinishedCallback) {
     var numDatasets = json.regionId.length;
-    var numInserted = 0;
+    var numUpdated = 0;
+    var tableName = "";
     db.transaction(function(tx) {
         for (var i = 0; i < numDatasets; i++) {
-            if (regionIds.indexOf(json.regionId[i]) == -1) {
-                tx.executeSql(`INSERT INTO BirdSubRegions VALUES (?,?,?,?)`,
-                    [json.regionId[i], json.speciesId[i], json.nonBreederInRegion[i], json.rareInRegion[i]], () => {
+            if (regionIds.indexOf(json.regionId[i]) == -1) tableName = "BirdSubRegions";
+            else tableName = "BirdRegions";
 
-                    if (++numInserted == numDatasets) onFinishedCallback();
-                });
-            }
-            else {
-                tx.executeSql(`INSERT INTO BirdRegions VALUES (?,?,?,?)`,
-                    [json.regionId[i], json.speciesId[i], json.nonBreederInRegion[i], json.rareInRegion[i]], () => {
+            _sqlQuery("SELECT COUNT(*) from " + tableName, [], {success: (tx,res) => {
+                if (res.rows._array[0]["COUNT(*)"] === 0) {
+                    _sqlQuery("INSERT INTO " + tableName + " VALUES (?,?,?,?)",
+                        [json.regionId[i], json.speciesId[i], json.nonBreederInRegion[i], json.rareInRegion[i]], {success: (tx,res) => {
+                            if (++numUpdated == numDatasets) onFinishedCallback();
+                    }},tx);
+                }
 
-                    if (++numInserted == numDatasets) onFinishedCallback();
-                });
-            }
+                else {
+                    var idName = ""
+                    if (tableName === "BirdSubRegions") idName = "subregion_id";
+                    else idName = "region_id";
+                    _sqlQuery("UPDATE " + tableName + " SET non_breeder=?,rare=? WHERE " + idName + "=?, bird_id=?",
+                        [json.nonBreederInRegion[i], json.rareInRegion[i], json.regionId[i], json.speciesId[i]], {success: (tx,res) => {
+                            if (++numUpdated == numDatasets) onFinishedCallback();
+                    }}, tx);
+                }
+            }},tx);
         }
     });
 };
@@ -147,41 +213,6 @@ var getBirdById = function (id, callbacks) {
         callbacks.success(result);
     }});
 }
-//Define id explicitly for birds to correspond to Dendroica's IDs
-var _insertBird = function (id, name, scientificName, rangeDescription, songDescription, callbacks) {
-    var query = `INSERT INTO Birds (_id, name, scientific_name, range_description, song_description) VALUES (?,?,?,?,?)`;
-    _sqlQuery(query, [id,name,scientificName,rangeDescription,songDescription], callbacks);
-};
-
-var _insertList = function (name, callbacks) {
-    var query = `INSERT INTO Lists (_id, name) VALUES (?,?)`;
-    _sqlQuery(query, [name], callbacks);
-};
-
-var _insertBirdImage = function (id, birdID, path, credits, displayOrder, callbacks) {
-    var query = `INSERT INTO BirdImages (_id, bird_id, filename, credits, display_order) VALUES (?,?,?,?,?)`;
-    _sqlQuery(query, [id, birdID, path, credits, displayOrder], callbacks);
-};
-
-var _insertMapImage = function (birdID, path, credits, callbacks) {
-    var query = `INSERT INTO MapImages (bird_id, filename, credits) VALUES (?,?,?)`;
-    _sqlQuery(query, [birdID, path, credits], callbacks);
-};
-
-var _insertVocalization = function (birdID, spectroPath, mp3Path, credits, callbacks) {
-    var query = `INSERT INTO Vocalizations (bird_id, filename, mp3_filename, credits) VALUES (?,?,?,?)`;
-    _sqlQuery(query, [birdID,spectroPath,mp3Path,credits], callbacks);
-};
-
-var _insertBirdRegion = function (regionID, birdID, nonBreeder, rare, callbacks) {
-    var query = `INSERT INTO BirdRegions (region_id, bird_id, non_breeder, rare) VALUES (?,?,?,?)`;
-    _sqlQuery(query, [regionID, birdID, nonBreeder, rare], callbacks);
-};
-
-var _insertBirdList = function (listID, birdID, callbacks) {
-    var query = `INSERT INTO BirdLists (list_id, bird_id) VALUES (?,?)`;
-    _sqlQuery(query, [listID, birdID], callbacks)
-};
 
 var _getRegionIDByName = function (name, callbacks) {
     var query = `SELECT _id from Regions where (name = ?)`;
@@ -220,7 +251,27 @@ var _sqlQuery = function (query, params, callbacks, tx) {
     });
 }
 
-var printDatabase = function() {
+var printTable = function(tableName, numRowsOrId, isIdConstraint, onFinishedCallback) {
+    var constraint = "";
+
+    if (numRowsOrId) {
+        if (!isIdConstraint || isIdConstraint === false) constraint = " LIMIT " + numRowsOrId;
+        else constraint = " WHERE _id=" + numRowsOrId;
+    }
+
+    _sqlQuery("SELECT COUNT(*) from " + tableName, [], { success: (tx,res) => { count = res.rows._array[0]["COUNT(*)"];
+        _sqlQuery("SELECT * from " + tableName + constraint, [], {success: (tx, res) => {
+            console.log(`----------------------------------- ${tableName} START ------------------------------------------------`);
+            res.rows._array.forEach(entry => {
+                console.log(JSON.stringify(entry, null, 2));
+            })
+            console.log("Number of Rows: " + count);
+            console.log(`----------------------------------- ${tableName} END---------------------------------------------------`);
+        }});
+    }});
+}
+
+var printDatabase = function(onFinishedCallback) {
     var count = 0;
 
      _printTable = (tableName, table, count) => {
@@ -281,7 +332,7 @@ var printDatabase = function() {
                                                     _sqlQuery('SELECT COUNT(*) from FileSubRegions', [], { success: (tx,res) => { count = res.rows._array[0]["COUNT(*)"];
                                                     _sqlQuery('SELECT * from FileSubRegions', [], {success: (tx, res) => {
                                                         _printTable("FileSubRegions", res.rows._array, count);
-
+                                                        if (onFinishedCallback) onFinishedCallback();
                                                     }},tx); }},tx);
                                                 }},tx); }},tx);
                                             }},tx); }},tx);
@@ -337,8 +388,8 @@ var initDB = function(onFinishedCallback) {
     var createTables = function() {
         var query;
         //User Table
-        query = `CREATE TABLE if not exists User (_id integer primary key not null unique, dendroica_id integer not null unique, username text, password text, email text, first_name text, last_name text, exclude_non_breeding boolean,
-        exclude_rare boolean, language text, naming text, sorting text)`
+        query = `CREATE TABLE if not exists User (_id integer primary key not null unique, dendroica_id integer not null unique, username text, password text, email text, first_name text, last_name text,
+        language text, naming text, sorting text)`
         _sqlQuery(query, [], {success: (tx,res) => {
             console.log("--> Created User Table");
             //Create Version Data Table
@@ -419,7 +470,9 @@ const DatabaseModule = {
     updateVersionData: updateVersionData,
     getBirdById: getBirdById,
     insertMultiple: insertMultiple,
-    insertBirdRegionsAndBirdSubRegions: insertBirdRegionsAndBirdSubRegions,
-    printDatabase: printDatabase
+    upsertMultiple: upsertMultiple,
+    updateInsertBirdRegionsAndBirdSubRegions: updateInsertBirdRegionsAndBirdSubRegions,
+    printDatabase: printDatabase,
+    printTable: printTable
 };
 export default DatabaseModule;
