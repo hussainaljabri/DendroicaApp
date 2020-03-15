b86b0dfafe06b1327f4dfaa3ccc66378761f2daa
hussainaljabri <ibmhusin@gmail.com>
2020-03-15 13:37:58 -0700

    Compining Imports of commons.

--- src/UI/Welcome.js
+++ src/UI/Welcome.js
@@ -1,74 +1,14 @@
 import React, {Component} from 'react';
-import OnboardingLogo from '../commons/OnboardingLogo';
 import {TextInput, TouchableOpacity, Alert, Animated, Image, View, Text, ActivityIndicator } from 'react-native';
 import { images } from '../constants/images';
 import Authentication from "../DB/Authentication";
 import DatabaseManagementModule from "../DB/DatabaseManagementModule";
 import MediaHandler from "../DB/MediaHandler";
+import '../commons/'; // InputBox, LoginButton, OnboardingLogo.
 
 
-const LoginButton = ({children, onPress}) =>(
-    <TouchableOpacity onPress={onPress}>
-        <View 
-        style={{
-            marginBottom: 5,
-            flexDirection: 'row',
-            backgroundColor: `darkgray`,
-            marginHorizontal: '10%',
-            justifyContent:"center",
-            alignItems: 'center',
-            padding: 15,
-            borderRadius: 5
-        }}>
-{/* <Image style={{height:20,width:20}} source={images.gmail} /> */}
-            {/* <View style={{marginRight: 5}} >
-                <View style={{ height: 32, width: 32, backgroundColor: 'white', radius: 5, justifyContent: 'center' ,position: 'relative'}}>
-                    
-                    <Text>Logo</Text>
-                </View>
-            </View> */}
-            
-            <View>
-                <Text style={{fontSize: 12, color: 'white', fontWeight: '600', letterSpacing:2}}>
-                    {children}
-                </Text>
-            </View>
-            
-        </View>
-    </TouchableOpacity>
-);
 
-const InputBox = ({children, onChange, value, type}) =>(
-    <View 
-    style={{
-        marginBottom: 5,
-        flexDirection: 'row',
-        backgroundColor: `darkgray`,
-        marginHorizontal: '10%',
-        justifyContent:"center",
-        alignItems: 'center',
-        padding: 15,
-        borderRadius: 5
-    }}>
-        <View style={{width:'100%'}}>
-            <TextInput
-                style={{ height: 40, borderColor: 'gray', color:"whitesmoke", borderWidth: 1, paddingHorizontal: 5}}
-                onChangeText={text => onChange(text)}
-                value={value}
-                placeholder={children}
-                placeholderTextColor='whitesmoke'
-                underlineColorAndroid="transparent"
-                secureTextEntry={type==='pass'? true: false}
-            />
-        </View>
-        {/* <View>
-            <Text style={{fontSize: 12, color: 'white', fontWeight: '600', letterSpacing:2}}>
-                {children}
-            </Text>
-        </View> */}
-        
-    </View>
-);
+
 
 
 class Welcome extends Component{
