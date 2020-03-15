c579168d2dba7fb4098f67054e8f4409923cb483
hussainaljabri <ibmhusin@gmail.com>
2020-03-15 13:36:45 -0700

    Separated TextInput and LoginButton into separate files for better front-end control and re-use.

---
+++ src/commons/InputBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Image, View, Text} from 'react-native';
+import {images} from '../constants/images';
+
+const InputBox = ({children, onChange, value, type}) =>(
+    <View 
+    style={{
+        marginBottom: 5,
+        flexDirection: 'row',
+        backgroundColor: `darkgray`,
+        marginHorizontal: '10%',
+        justifyContent:"center",
+        alignItems: 'center',
+        padding: 15,
+        borderRadius: 5
+    }}>
+        <View style={{width:'100%'}}>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', color:"whitesmoke", borderWidth: 1, paddingHorizontal: 5}}
+                onChangeText={text => onChange(text)}
+                value={value}
+                placeholder={children}
+                placeholderTextColor='whitesmoke'
+                underlineColorAndroid="transparent"
+                secureTextEntry={type==='pass'? true: false}
+            />
+        </View>
+        {/* <View>
+            <Text style={{fontSize: 12, color: 'white', fontWeight: '600', letterSpacing:2}}>
+                {children}
+            </Text>
+        </View> */}
+        
+    </View>
+);
+
+export default InputBox;
